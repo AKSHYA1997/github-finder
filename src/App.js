30ae8b8249553c58e6a668fb279b19378d1731da
Akshya Chettiar <Akshya.Chettiar@nasdaq.com>
2021-12-10 16:07:18 +0530

    :tada initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      // start of JXS
+      <div className='App'>
+        <h1>Hello From The Other Side!</h1>
+      </div>
+      // end of JXS
+    </React.Fragment>
   );
 }
+
+// other way to code
+// class App extends React.Component {
+//   render() {
+//     return React.createElement(
+//       'div',
+//       { className: 'App' },
+//       React.createElement('h1', null, 'Hello From The Other Side!')
+//     );
+//   }
+// }
 
 export default App;
