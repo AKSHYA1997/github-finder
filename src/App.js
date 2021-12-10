ad14da9b1f5e817c1609070d43ea6901d3b4bd03
AKSHYA1997 <akshyaravishankar@hmail.com>
2021-12-10 16:30:21 +0530

    this commit is only for reference

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  const name = "Joe Blue";
+  const foo = () => "Blue Joe";
+  const loading = false;
+
   return (
     <React.Fragment>
-      // start of JXS
-      <div className='App'>
+      {/* start of JXS  */}
+      <div className="App">
         <h1>Hello From The Other Side!</h1>
+        <h1>Hello {name}</h1>
+        <h1>Hello {1 + 2}</h1>
+        <h1>Hello {foo()}</h1>
+        {loading ? <h1>loading...</h1> : <h1>Hi There😎</h1>}
       </div>
-      // end of JXS
+      {/* end of JXS */}
     </React.Fragment>
   );
 }
