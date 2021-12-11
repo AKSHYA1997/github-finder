a8010f686a0e6d52746d3c95318b6ad1687337a6
AKSHYA1997 <akshyaravishankar@hmail.com>
2021-12-11 14:05:26 +0530

    added react route dom

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,10 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 const Navbar = ({ title }) => {
   return (
     <nav className="navbar bg-primary">
       <h1>{title}</h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
     </nav>
   );
 };
