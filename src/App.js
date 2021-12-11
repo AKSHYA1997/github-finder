0a26fff2685b20ed523aad490fb843e996d3d879
AKSHYA1997 <akshyaravishankar@hmail.com>
2021-12-11 13:02:28 +0530

    added search feature

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
+import Search from "./components/users/Search";
 import axios from "axios";
 import "./App.css";
 class App extends Component {
@@ -19,12 +20,24 @@ class App extends Component {
 
     this.setState({ users: res.data, loading: false });
   }
+
+  searchUsers = async (text) => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    console.log(res.data);
+
+    this.setState({ users: res.data.items, loading: false });
+  };
   render() {
     return (
       <div className="App">
         <Navbar />
         {/* <UserItem /> */}
         <div className="container">
+          <Search searchUsers={this.searchUsers} />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
---
+++ src/components/users/Search.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+class Search extends Component {
+  state = {
+    text: "",
+  };
+
+  static propTypes = {
+    search: PropTypes.func.isRequired,
+  };
+
+  onChange = (e) => {
+    // this.setState({ text: e.target.value });
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.searchUsers(this.state.text);
+    this.setState({ text: "" });
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.onSubmit}>
+          <input
+            type="text"
+            name="text"
+            placeholder="Search Users ..."
+            value={this.state.text}
+            onChange={this.onChange}
+          />
+          <input
+            type="submit"
+            value="Search"
+            className="btn btn-dark btn-block"
+          />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Search;
