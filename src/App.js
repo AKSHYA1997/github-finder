93c4e94d9111b37a682959a656f08a6d036b267c
AKSHYA1997 <akshyaravishankar@hmail.com>
2021-12-11 13:21:40 +0530

    added clear users feature

--- src/App.js
+++ src/App.js
@@ -10,16 +10,16 @@ class App extends Component {
     loading: false,
   };
 
-  async componentDidMount() {
-    this.setState({ loading: true });
+  // async componentDidMount() {
+  //   this.setState({ loading: true });
 
-    const res = await axios.get(
-      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
-    console.log(res.data);
+  //   const res = await axios.get(
+  //     `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+  //   );
+  //   console.log(res.data);
 
-    this.setState({ users: res.data, loading: false });
-  }
+  //   this.setState({ users: res.data, loading: false });
+  // }
 
   searchUsers = async (text) => {
     this.setState({ loading: true });
@@ -31,14 +31,24 @@ class App extends Component {
 
     this.setState({ users: res.data.items, loading: false });
   };
+
+  clearUsers = () => {
+    this.setState({ users: [], loading: false });
+  };
+
   render() {
+    const { users, loading } = this.state;
     return (
       <div className="App">
         <Navbar />
         {/* <UserItem /> */}
         <div className="container">
-          <Search searchUsers={this.searchUsers} />
-          <Users loading={this.state.loading} users={this.state.users} />
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={users.length > 0 ? true : false}
+          />
+          <Users loading={loading} users={users} />
         </div>
       </div>
     );
